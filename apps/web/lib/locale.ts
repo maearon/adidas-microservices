98fd24defa547854c60f8cb65154ecf6a83b44c7
maearon <manhng132@gmail.com>
2025-07-17 18:37:52 +0700

    adjust

--- apps/web/lib/locale.ts
+++ apps/web/lib/locale.ts
@@ -3,7 +3,6 @@ import heroEn from "@/locales/en-US/hero.json"
 import heroVi from "@/locales/vi-VN/hero.json"
 import headerEn from "@/locales/en-US/header.json"
 import headerVi from "@/locales/vi-VN/header.json"
-import { cookies } from 'next/headers'
 
 const locales = {
   "en-US": {
@@ -22,8 +21,3 @@ type Namespace = keyof typeof locales["en-US"]
 export function getTranslations(locale: Locale, namespace: Namespace) {
   return locales[locale][namespace]
 }  
-
-export function getLocaleFromCookies() {
-  const cookieStore = cookies()
-  return cookieStore.get('NEXT_LOCALE')?.value ?? 'en-US'
-}
