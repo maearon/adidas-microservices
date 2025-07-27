092ece87900f25c6f1ba5bc8d4e843eaa05b0f07
maearon <manhng132@gmail.com>
2025-07-27 13:35:51 +0700

    text base promo

--- apps/web/components/home/PromoBanner.tsx
+++ apps/web/components/home/PromoBanner.tsx
@@ -10,7 +10,7 @@ export default function PromoBanner() {
       className="block w-full bg-[#e9edf0] hover:opacity-90 transition"
     >
       <div className="relative flex items-center px-4 pt-3 pb-4 md:px-6 md:pt-3 md:pb-4">
-        <p className="text-xs md:text-base text-gray-800 mx-auto pr-8 md:pr-10">
+        <p className="text-base text-gray-800 mx-auto pr-8 md:pr-10">
           Get a free backpack with orders $150+, while supplies last.
         </p>
         <span className="text-2xl absolute right-4 md:right-6">→</span>
