5152970ba8e2ff4f320276de712f900ea11728fd
maearon <manhng132@gmail.com>
2025-07-29 15:31:53 +0700

    seed pink tubular shadow shoes avatar and hover

--- apps/ruby-rails-boilerplate/db/seeds/seed-after-variant-hover-image.rb
+++ apps/ruby-rails-boilerplate/db/seeds/seed-after-variant-hover-image.rb
@@ -1,24 +1,24 @@
 product = Product.find(3)
 
 # 🧹 Xoá ảnh cũ nếu có
-product.image.purge if product.image.attached?
-product.hover_image.purge if product.hover_image.attached?
+product.variants.find_by(id: 462).avatar.purge if product.image.attached?
+product.variants.find_by(id: 462).hover.purge if product.hover_image.attached?
 
 # 📁 Đường dẫn đến thư mục thumbnail
-thumbnail_dir = Rails.root.join("app/assets/images/products/94/thumbnail")
+thumbnail_dir = Rails.root.join("app/assets/images/products/93/thumbnail")
 
 # 📄 Lấy danh sách ảnh và sắp xếp
 image_files = Dir.glob("#{thumbnail_dir}/*.jpg").sort_by { |path| File.basename(path).downcase }
 
 # ✅ Gắn ảnh nếu đủ file
 if image_files[0] && image_files[1]
-  product.variants.find_by(id: 466).avatar.attach(
+  product.variants.find_by(id: 462).avatar.attach(
     io: File.open(image_files[0]),
     filename: File.basename(image_files[0]),
     content_type: "image/jpeg"
   )
 
-  product.variants.find_by(id: 466).hover.attach(
+  product.variants.find_by(id: 462).hover.attach(
     io: File.open(image_files[1]),
     filename: File.basename(image_files[1]),
     content_type: "image/jpeg"
@@ -26,3 +26,31 @@ if image_files[0] && image_files[1]
 else
   puts "❌ Không tìm thấy đủ ảnh trong #{thumbnail_dir}"
 end
+# product = Product.find(3)
+
+# # 🧹 Xoá ảnh cũ nếu có
+# product.variants.find_by(id: 466).avatar.purge if product.image.attached?
+# product.variants.find_by(id: 466).hover.purge if product.hover_image.attached?
+
+# # 📁 Đường dẫn đến thư mục thumbnail
+# thumbnail_dir = Rails.root.join("app/assets/images/products/94/thumbnail")
+
+# # 📄 Lấy danh sách ảnh và sắp xếp
+# image_files = Dir.glob("#{thumbnail_dir}/*.jpg").sort_by { |path| File.basename(path).downcase }
+
+# # ✅ Gắn ảnh nếu đủ file
+# if image_files[0] && image_files[1]
+#   product.variants.find_by(id: 466).avatar.attach(
+#     io: File.open(image_files[0]),
+#     filename: File.basename(image_files[0]),
+#     content_type: "image/jpeg"
+#   )
+
+#   product.variants.find_by(id: 466).hover.attach(
+#     io: File.open(image_files[1]),
+#     filename: File.basename(image_files[1]),
+#     content_type: "image/jpeg"
+#   )
+# else
+#   puts "❌ Không tìm thấy đủ ảnh trong #{thumbnail_dir}"
+# end
