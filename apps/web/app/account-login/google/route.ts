3779d7a2423f432538986bf34d69052e55c75065
mark <manhng132@gmail.com>
2025-08-03 13:30:59 +0700

    Update route.ts

--- apps/web/app/account-login/google/route.ts
+++ apps/web/app/account-login/google/route.ts
@@ -1,28 +1,19 @@
 import { google } from "@/lib/social-login/google";
-import { generateCodeVerifier, generateState } from "arctic";
+import { generateState } from "arctic";
 import { cookies } from "next/headers";
 
 export async function GET() {
   const state = generateState();
-  const codeVerifier = generateCodeVerifier();
 
-  // Tạo URL đăng nhập với Google
-  const url = await google.createAuthorizationURL(state, codeVerifier, {
+  // ✅ Không dùng codeVerifier vì Google không hỗ trợ PKCE cho Web OAuth
+  const url = await google.createAuthorizationURL(state, {
     scopes: ["profile", "email"],
   });
 
-  // Lưu state và code_verifier vào cookie để kiểm tra khi callback về
   const secure = process.env.NODE_ENV === "production";
 
+  // ✅ Chỉ lưu state
   cookies().set("state", state, {
-    path: "/",
-    secure,
-    httpOnly: true,
-    maxAge: 60 * 10,
-    sameSite: "lax",
-  });
-
-  cookies().set("code_verifier", codeVerifier, {
     path: "/",
     secure,
     httpOnly: true,
