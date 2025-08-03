6a2051c4740d673f2ca89a8e5639f6f16ce13776
mark <manhng132@gmail.com>
2025-08-03 13:24:27 +0700

    Update google.ts

--- apps/web/lib/social-login/google.ts
+++ apps/web/lib/social-login/google.ts
@@ -1,12 +1,7 @@
-// apps/web/lib/social-login/google.ts 
-// https://console.cloud.google.com/apis/credentials?inv=1&invt=Ab4c6w&project=shop-php-463700
-
 import { Google } from "arctic";
 
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  `${process.env.PUBLIC_BASE_URL}/api/auth/callback/google`,
+  "https://adidas-mocha.vercel.app/api/auth/callback/google",
 );
-
-google.scopes = ['openid', 'email', 'profile'];
