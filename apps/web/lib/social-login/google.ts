5062c0258cc9dff1a2286e7dbd064a41529b58fb
mark <manhng132@gmail.com>
2025-08-03 13:07:59 +0700

    Update google.ts

--- apps/web/lib/social-login/google.ts
+++ apps/web/lib/social-login/google.ts
@@ -6,7 +6,7 @@ import { Google } from "arctic";
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  `${process.env.GOOGLE_REDIRECT_URI}/api/auth/callback/google`,
+  `${process.env.PUBLIC_BASE_URL}/api/auth/callback/google`,
 );
 
 google.scopes = ['openid', 'email', 'profile'];
