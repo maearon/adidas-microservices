dcc632498240d3ea6bdbef8af6a0e02687beca94
mark <manhng132@gmail.com>
2025-08-04 16:58:36 +0700

    Update google.ts

--- apps/web/lib/social-login/google.ts
+++ apps/web/lib/social-login/google.ts
@@ -1,3 +1,6 @@
+// apps/web/lib/social-login/google.ts 
+// https://console.cloud.google.com/apis/credentials?inv=1&invt=Ab4c6w&project=shop-php-463700
+
 import { Google } from "arctic";
 
 export const google = new Google(
