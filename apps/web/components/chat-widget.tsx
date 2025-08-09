f2c206b3080c488f86959eab0286d845ed71959b
mark <manhng132@gmail.com>
2025-08-09 15:08:28 +0700

    Update chat-widget.tsx

--- apps/web/components/chat-widget.tsx
+++ apps/web/components/chat-widget.tsx
@@ -52,7 +52,7 @@ export default function ChatWidget() {
 
   useEffect(() => {
     scrollToBottom()
-  }, [messages])
+  }, [messages, isMinimized, isOpen])
 
   // Initialize socket connection
   useEffect(() => {
