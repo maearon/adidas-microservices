22fde11e3b74c4cc9d299f29504b4e4b93a8a450
maearon <manhng132@gmail.com>
2025-08-16 05:56:20 +0700

    move db drizzle to src

--- apps/web/src/app/api/auth/sign-in/route.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from "@/lib/auth";
-import { toNextJsHandler } from "better-auth/next-js";
-
-const handler = toNextJsHandler(auth.handler);
-
-// xuất các method hay dùng (đặc biệt là GET, POST, OPTIONS nếu có preflight)
-export { handler as GET, handler as POST, handler as OPTIONS };
