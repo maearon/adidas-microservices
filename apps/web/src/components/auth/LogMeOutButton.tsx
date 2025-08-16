dd9b4f2f0465a22f225d7ed58b1e316f64c33b68
mark <manhng132@gmail.com>
2025-08-16 21:04:59 +0700

    Update LogMeOutButton.tsx

--- apps/web/src/components/auth/LogMeOutButton.tsx
+++ apps/web/src/components/auth/LogMeOutButton.tsx
@@ -33,6 +33,7 @@ export const LogMeOutButton = () => {
     const name = cookie.split("=")[0].trim();
     document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
   });
+window.location.reload();
         },
         onError: (err) => {
           console.error("Logout failed", err);
