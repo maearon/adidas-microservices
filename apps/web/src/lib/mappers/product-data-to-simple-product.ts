0b4ce0e5a624d942ec9b59a3472149a4db1e40fe
maearon <manhng132@gmail.com>
2025-08-28 16:03:58 +0700

    mapProductDataToSimpleProduct variants [ ]

--- apps/web/src/lib/mappers/product-data-to-simple-product.ts
+++ apps/web/src/lib/mappers/product-data-to-simple-product.ts
@@ -7,18 +7,7 @@ export function mapProductDataToSimpleProduct(productData: any): Product {
     sport: productData.sport || '',
     model_number: productData.model_number,
     price: productData.variants[0].price || productData.price || 0,
-    variants: productData.variants?.map((v: any) => ({
-      variant_code: v.variant_code,
-      avatar_url: v?.avatar_url ||
-        v?.image_urls?.[0] ||
-        productData.main_image_url ||
-        "/placeholder.png",
-      hover_url: v?.hover_url ||
-        v?.image_urls?.[2] ||
-        productData.hover_image_url ||
-        "/placeholder.png",
-      image_urls: v?.images ?? [],
-    })) || [],
+    variants: [],
     image_url: productData?.variants[0]?.avatar_url ||
         productData?.variants[0]?.image_urls?.[0] ||
         productData.main_image_url ||
