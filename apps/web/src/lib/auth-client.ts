63a9627e18e0ab0be24330ac366beec2affa0664
maearon <manhng132@gmail.com>
2025-09-12 15:15:10 +0700

    sign-up

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,11 @@
-import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { nextCookies } from "better-auth/next-js";
+import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
+import { auth } from "./auth";
  
 export const authClient = createAuthClient({
-    //you can pass client configuration here
+  plugins: [inferAdditionalFields<typeof auth>(), nextCookies()],
+  //you can pass client configuration here
 });
 
 // Lấy type provider từ chính hàm signIn.social
