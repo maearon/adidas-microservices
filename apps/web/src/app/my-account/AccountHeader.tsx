16cf41b6fdadf321868cd1a61fd223a37ca498de
maearon <manhng132@gmail.com>
2025-09-18 17:17:37 +0700

    text-black

--- apps/web/src/app/my-account/AccountHeader.tsx
+++ apps/web/src/app/my-account/AccountHeader.tsx
@@ -13,13 +13,13 @@ export default function AccountHeader({ session }: AccountHeaderProps) {
   return (
     <div className="flex items-center justify-between border-b border-[#D3D7DA]">
       <div>
-        <h1 className="text-3xl font-bold">HI {session?.user?.name?.toUpperCase() || "USER"}</h1>
+        <h1 className="text-3xl font-bold text-black">HI {session?.user?.name?.toUpperCase() || "USER"}</h1>
         <div className="flex items-center mt-2">
           <span className="text-base text-gray-600 mr-2">👑</span>
           <span className="text-base text-gray-600">0 points to spend</span>
         </div>
       </div>
-      <div className="text-right">
+      <div className="text-right text-black">
         <div className="text-2xl font-bold">adiclub</div>
         <div className="text-base">LEVEL 1</div>
       </div>
