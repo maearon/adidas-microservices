777d92463a03cf55b57fc7410ed9054e57264a6c
maearon <manhng132@gmail.com>
2025-09-18 16:54:12 +0700

    redirect("/sign-in")

--- apps/web/src/app/my-account/page.tsx
+++ apps/web/src/app/my-account/page.tsx
@@ -1,11 +1,12 @@
 import { type Session } from "@/lib/auth"
 import { getServerSession } from "@/lib/get-session";
 import MyAccountClient from "./MyAccountClient";
+import { redirect } from "next/navigation";
 
 const MyAccountPage = async () => {
   const session: Session | null = await getServerSession() // Session type-safe
 
-  if (!session?.user?.email) return null
+  if (!session?.user?.email) redirect("/sign-in")
 
   return (
     <MyAccountClient session={session} />
