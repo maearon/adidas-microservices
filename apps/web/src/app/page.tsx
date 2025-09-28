c5939ad85b20905df991f9fd0511428c9b9ee79f
maearon <manhng132@gmail.com>
2025-09-28 18:53:21 +0700

    LandingPageContents simple from server page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,5 @@
-import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import LandingPageClient from "./LandingPageClient";
+import { LandingPageContents } from "./_ui/landing-page-contents";
 
-const LandingPage = async () => {
-  const queryClient = new QueryClient();
-  // ... (nếu cần prefetch query thì thêm ở đây)
-  const dehydratedState = dehydrate(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydratedState}>
-      <LandingPageClient />
-    </HydrationBoundary>
-  );
-};
-
-export default LandingPage;
+export default function LandingPage() {
+  return <LandingPageContents />;
+}
