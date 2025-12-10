3028ebd5f885c31c49d7fb72357004a3c4133b96
maearon <manhng132@gmail.com>
2025-12-10 15:38:50 +0700

    fix can not send email

--- apps/web/src/lib/email.ts
+++ apps/web/src/lib/email.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+
 interface SendEmailValues {
   to: string;
   subject: string;
@@ -7,29 +8,19 @@ interface SendEmailValues {
 
 function createTransporter() {
   return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
+    service: "gmail",
     auth: {
-      user: "manhng132@gmail.com",
-      pass: "rqisoolrehrwayum",
+      user: process.env.SMTP_USERNAME!,
+      pass: process.env.SMTP_PASSWORD!,
     },
-    from: "manhng132@gmail.com",
   });
 }
 
 export async function sendEmail({ to, subject, text }: SendEmailValues) {
-  // const smtpPassword = process.env.SMTP_PASSWORD;
-  
-  // if (!smtpPassword) {
-  //   console.error("❌ smtp password not found! Please set SMTP_PASSWORD environment variable.");
-  //   console.error("Expected format: rqi_yum");
-  //   throw new Error("Smtp Password Key not configured");
-  // }
-
   const transporter = createTransporter();
-  
+
   await transporter.sendMail({
-    from: process.env.SMTP_USERNAME,
+    from: process.env.SMTP_USERNAME!,
     to,
     subject,
     text,
