838c3c9c3b1450dc96bdfedec7ce979abb8457f1
maearon <manhng132@gmail.com>
2025-12-10 16:07:37 +0700

    fix send email

--- apps/web/src/lib/email.ts
+++ apps/web/src/lib/email.ts
@@ -17,6 +17,14 @@ function createTransporter() {
 }
 
 export async function sendEmail({ to, subject, text }: SendEmailValues) {
+  // const smtpPassword = process.env.SMTP_PASSWORD;
+  
+  // if (!smtpPassword) {
+  //   console.error("❌ smtp password not found! Please set SMTP_PASSWORD environment variable.");
+  //   console.error("Expected format: rqi_yum");
+  //   throw new Error("Smtp Password Key not configured");
+  // }
+  
   const transporter = createTransporter();
 
   await transporter.sendMail({
